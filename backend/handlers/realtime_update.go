35e56c33b211d5ddb0f7400d571b1b0b91f02306
okkulkarni2001 <kulkarnionkar1911@gmail.com>
2025-08-19 19:32:36 -0400

    feat(realtime): in-memory pub/sub for response updates

---
+++ backend/handlers/realtime_update.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"sync"
+)
+
+var (
+	rtMu sync.Mutex
+	// key = formId hex; value = set of subscriber channels
+	rtSubs = map[string]map[chan struct{}]struct{}{}
+)
+
+func rtSubscribe(formID string) (ch chan struct{}, unsubscribe func()) {
+	ch = make(chan struct{}, 1)
+
+	rtMu.Lock()
+	if _, ok := rtSubs[formID]; !ok {
+		rtSubs[formID] = make(map[chan struct{}]struct{})
+	}
+	rtSubs[formID][ch] = struct{}{}
+	rtMu.Unlock()
+
+	unsubscribe = func() {
+		rtMu.Lock()
+		if set, ok := rtSubs[formID]; ok {
+			delete(set, ch)
+			if len(set) == 0 {
+				delete(rtSubs, formID)
+			}
+		}
+		rtMu.Unlock()
+		close(ch)
+	}
+	return ch, unsubscribe
+}
+
+func rtNotify(formID string) {
+	rtMu.Lock()
+	subs := rtSubs[formID]
+	// fan-out non-blocking
+	for ch := range subs {
+		select {
+		case ch <- struct{}{}:
+		default:
+		}
+	}
+	rtMu.Unlock()
+}
